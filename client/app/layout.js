c90acb3373a6f426731658a5c6231ca0fb94410e
thanhnq <quocthanhn87@gmial.com>
2024-08-26 22:16:18 +0700

    commit to pull

--- client/app/layout.js
+++ client/app/layout.js
@@ -14,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
