fd6f0e91b6249cf4eb85051614f96f3c0e64cd2e
Cao Hoàng Thành <160160995+ThanhDevLagi@users.noreply.github.com>
2024-08-26 22:23:17 +0700

    fix chi nhanhs

--- client/app/admin/chi-nhanh/[id]/page.js
+++ client/app/admin/chi-nhanh/[id]/page.js
@@ -1,10 +1,7 @@
-import { useRouter } from 'next/router';
 
 function ChiNhanhDetail() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <div>Chi nhánh chi tiết cho ID: {id}</div>;
+  
+  return <div>Chi nhánh chi tiết cho ID: </div>;
 }
 
 export default ChiNhanhDetail;
