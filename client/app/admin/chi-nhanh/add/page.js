6a617df6aa235b54062596e3edc1907b3f2edf44
Cao Hoàng Thành <160160995+ThanhDevLagi@users.noreply.github.com>
2024-08-28 22:25:28 +0700

    thêm hiệu ứng và chức năng bên chi nhánh (chưa hoàn thiện)

---
+++ client/app/admin/chi-nhanh/add/page.js
@@ -0,0 +1,48 @@
+"use client";
+
+import AdminHeader from "@/app/_components/AdminHeader";
+import AdminThemChiNhanhImg from "@/app/_components/AdminThemChiNhanhImg";
+import AdminThemChiNhanhInput from "@/app/_components/AdminThemChiNhanhInput";
+import { Heading, Stack, Text } from "@chakra-ui/react";
+
+const fields = [
+  { type: 'text', placeholder: 'Tên chi nhánh' },
+  { type: 'text', placeholder: 'Địa chỉ' },
+  { type: 'text', placeholder: 'Email' },
+  { type: 'text', placeholder: 'Số điện thoại' },
+];
+
+const fields2 = [
+  { type: 'textarea', placeholder: 'Slogan' },
+  { type: 'textarea', placeholder: 'Mô tả' },
+];
+
+const fields3 = [
+  { type: 'textarea', placeholder: 'Mô tả' },
+];
+
+function ChiNhanhAddPage() {
+  return (
+    <div>
+      <AdminHeader showSearchForm={false} title={'Chi tiết chi nhánh'} />
+      <Stack alignItems="start" spacing="8" direction={'row'} className='mt-5'>
+        <Heading as='h1' size='lg'>Chi nhánh / </Heading>
+        <Text fontSize='5xl'>Thêm chi nhánh</Text>
+      </Stack>
+      <div className="flex gap-5 w-full mt-[30px]">
+          <AdminThemChiNhanhInput fields={fields} title='Thông tin liên hệ'/>
+          <AdminThemChiNhanhImg title='Hình ảnh carousel' inputId="image-upload-carousel"></AdminThemChiNhanhImg>
+      </div>
+      <div className="flex gap-5 w-full mt-[30px]">
+          <AdminThemChiNhanhInput fields={fields2} type={'textarea'} title='Slogan & Mô tả'/>
+          <AdminThemChiNhanhImg title='Hình ảnh mô tả' inputId="image-upload-description"></AdminThemChiNhanhImg>
+      </div>
+      <div className="flex gap-5 w-full mt-[30px]">
+          <AdminThemChiNhanhInput fields={fields3} typeADD={'textarea'} placeholderADD='Mô tả' title='Trang thiết bị'/>
+          <AdminThemChiNhanhImg title='Hình ảnh sơ đồ' inputId="image-upload-map"></AdminThemChiNhanhImg>
+      </div>
+    </div>
+  );
+}
+
+export default ChiNhanhAddPage;
--- client/app/admin/chi-nhanh/page.js
+++ client/app/admin/chi-nhanh/page.js
@@ -1,5 +1,7 @@
-import { Grid, Card, Image, Text, Stack, GridItem, Heading, Box } from '@chakra-ui/react';
 
+import { Grid, Card, Image, Text, Stack, GridItem, Heading, Box } from '@chakra-ui/react';
+import { Col } from 'antd';
+import Link from 'next/link';
 export const metadata = {
   title: 'Chi nhánh',
 };
@@ -7,64 +9,44 @@ export const metadata = {
 function PageChiNhanh() {
   return (
     <Box>
-      <Heading>Chi nhánh</Heading>
+      <Heading className='text-2xl font-bold leading-8'>Chi nhánh</Heading>
+      <Stack alignItems="start" spacing="8" direction={'row'} className='mt-5' >
+        <Heading as='h1' size='lg' className=''>Chi nhánh / </Heading>
+      </Stack>
       <Grid
-        templateColumns='repeat(3, 1fr)'
-        gap='20'
-        className='mt-8'
+        templateColumns="repeat(3, 1fr)"
+        gap="20"
+        className="mt-8"
       >
-        <GridItem w='100%' className='rounded-lg'>
-          <Card maxW='sm'>
+        <GridItem w="100%" className="rounded-lg product-card">
+          <Card maxW="sm" className="overflow-hidden relative group">
             <Image
-              src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-              alt='Green double couch with wooden legs'
-              borderRadius='sm'
-              h='200px'
-              w='100%'
-              className='relative rounded-lg'
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              h="200px"
+              w="100%"
+              className="rounded-lg "
             />
-            <Stack alignItems='center' spacing='3' className='absolute bottom-5 left-0 right-0'>
-              <Text color='white' fontSize='2xl' py='8' px='10' bg='blue' className='rounded-lg'>
+            <Stack
+              alignItems="center"
+              spacing="3"
+              className="absolute bottom-0 left-0 right-0 transform"
+            >
+              <Text color='white' fontSize='2xl' py='8' px='10' className='bg-blackAlpha-600 rounded-lg transition-transform duration-300 group-hover:-translate-y-6'>
                 Hoàng Văn Thụ
               </Text>
-            </Stack>
-          </Card>
-        </GridItem>
-        <GridItem w='100%' className='rounded-lg'>
-          <Card maxW='sm'>
-            <Image
-              src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-              alt='Green double couch with wooden legs'
-              borderRadius='sm'
-              h='200px'
-              w='100%'
-              className='relative rounded-lg'
-            />
-            <Stack alignItems='center' spacing='3' className='absolute bottom-5 left-0 right-0'>
-              <Text color='white' fontSize='2xl' py='8' px='10' bg='blue' className='rounded-lg'>
-                Hoàng Văn Thụ
-              </Text>
-            </Stack>
-          </Card>
-        </GridItem>
-        <GridItem w='100%' className='rounded-lg'>
-          <Card maxW='sm'>
-            <Image
-              src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-              alt='Green double couch with wooden legs'
-              borderRadius='sm'
-              h='200px'
-              w='100%'
-              className='relative rounded-lg'
-            />
-            <Stack alignItems='center' spacing='3' className='absolute bottom-5 left-0 right-0'>
-              <Text color='white' fontSize='2xl' py='8' px='10' bg='blue' className='rounded-lg'>
-                Hoàng Văn Thụ
-              </Text>
+              <Link href='' className='translate-y-full transition-transform duration-300 group-hover:-translate-y-4 w-fit bg-white text-black font-medium px-6 py-2 rounded-xl '>
+                Chi tiết
+              </Link>
             </Stack>
           </Card>
         </GridItem>
       </Grid>
+      <div className='h-48 flex w-full rounded-lg items-center justify-center bg-whiteAlpha-100 mt-5'>
+        <Link href='/admin/chi-nhanh/add' className='p-5 size-5 flex items-center justify-center rounded-full text-xl font-semibold   border hover:bg-whiteAlpha-400 hover:border-none transition-all'>
+          +
+        </Link>
+      </div>
     </Box>
   );
 }
