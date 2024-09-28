cc779898135e5ac37acbeace799c8ef85079d4de
Cao Hoàng Thành <160160995+ThanhDevLagi@users.noreply.github.com>
2024-09-28 16:03:36 +0700

    add Button cho trang quản lý tiệc và components

--- client/app/admin/quan-ly-tiec/[slug]/[id]/page.js
+++ client/app/admin/quan-ly-tiec/[slug]/[id]/page.js
@@ -7,6 +7,7 @@ import IconButton from '@/app/_components/IconButton';
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import IconButtonSave from '@/app/_components/IconButtonSave';
 import TableDetail from '@/app/_components/TableDetailCost';
+import ButtonCustomAdmin from '@/app/_components/ButtonCustomAdmin';
 
 const TitleSpanInfo = ({ title }) => (
     <span className="font-semibold text-xl leading-7 text-gray-600">{title}</span>
@@ -337,13 +338,63 @@ const ChiTietTiecCuaChiNhanhPage = ({ params }) => {
     { service: 'VAT', description: '10%', cost: '99.000.000' },
     { service: 'Tổng chi phí bao gồm VAT', description: '', cost: '1.000.000.000' },
     ];
+    const buttons = [
+        {
+            title: 'Xem hợp đồng',
+            svg: (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                <path d="M11.6665 0H2.33317V1.16667H11.6665V0ZM2.33317 14H11.6665V12.8333H2.33317V14ZM11.6665 2.33333H2.33317C1.6915 2.33333 1.1665 2.85833 1.1665 3.5V10.5C1.1665 11.1417 1.6915 11.6667 2.33317 11.6667H11.6665C12.3082 11.6667 12.8332 11.1417 12.8332 10.5V3.5C12.8332 2.85833 12.3082 2.33333 11.6665 2.33333ZM6.99984 3.9375C7.72317 3.9375 8.31234 4.52667 8.31234 5.25C8.31234 5.97333 7.72317 6.5625 6.99984 6.5625C6.2765 6.5625 5.68734 5.97333 5.68734 5.25C5.68734 4.52667 6.2765 3.9375 6.99984 3.9375ZM9.9165 9.91667H4.08317V9.04167C4.08317 8.0675 6.02567 7.58333 6.99984 7.58333C7.974 7.58333 9.9165 8.0675 9.9165 9.04167V9.91667Z" fill="white"/>
+              </svg>),
+            id: '',
+            bg: 'bg-teal-400',
+            textColor: 'text-white'
 
+        },
+        {
+            title: 'Xuất hợp đồng',
+            svg: (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.84375 3.0625H11.1576C11.563 3.06371 11.9514 3.22529 12.2381 3.51195C12.5247 3.79861 12.6863 4.18705 12.6875 4.59244L12.6875 4.59375L12.6875 8.96875L12.6875 8.97006C12.6863 9.37545 12.5247 9.76389 12.2381 10.0506C11.9514 10.3372 11.563 10.4988 11.1576 10.5L11.1562 10.5H10.5V9.625H11.1556C11.3296 9.62431 11.4963 9.55489 11.6193 9.43183C11.7424 9.30878 11.8118 9.14209 11.8125 8.96808V4.59442C11.8118 4.42041 11.7424 4.25372 11.6193 4.13067C11.4963 4.0076 11.3295 3.93817 11.1555 3.9375H2.84449C2.67046 3.93817 2.50374 4.0076 2.38067 4.13067C2.2576 4.25374 2.18817 4.42046 2.1875 4.59449V8.96801C2.18817 9.14204 2.2576 9.30876 2.38067 9.43183C2.50372 9.55489 2.67041 9.62431 2.84442 9.625H3.5V10.5H2.84375L2.84244 10.5C2.43705 10.4988 2.04861 10.3372 1.76195 10.0506C1.47529 9.76389 1.31371 9.37545 1.3125 8.97006L1.3125 8.96875V4.59244C1.31371 4.18705 1.47529 3.79861 1.76195 3.51195C2.04861 3.22529 2.43705 3.06371 2.84244 3.0625L2.84375 3.0625Z" fill="white"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.165 7C4.03936 7 3.9375 7.10186 3.9375 7.2275V11.585C3.9375 11.7106 4.03936 11.8125 4.165 11.8125H9.835C9.96064 11.8125 10.0625 11.7106 10.0625 11.585V7.2275C10.0625 7.10186 9.96064 7 9.835 7H4.165ZM3.0625 7.2275C3.0625 6.61861 3.55611 6.125 4.165 6.125H9.835C10.4439 6.125 10.9375 6.61861 10.9375 7.2275V11.585C10.9375 12.1939 10.4439 12.6875 9.835 12.6875H4.165C3.55611 12.6875 3.0625 12.1939 3.0625 11.585V7.2275Z" fill="white"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.59375 1.3125H9.40756C9.81295 1.31371 10.2014 1.47529 10.4881 1.76195C10.7747 2.04861 10.9363 2.43705 10.9375 2.84244L10.9375 2.84375L10.9375 3.5H10.0625V2.84442C10.0618 2.67041 9.99239 2.50372 9.86933 2.38067C9.74626 2.2576 9.57954 2.18817 9.40551 2.1875H4.59449C4.42046 2.18817 4.25374 2.2576 4.13067 2.38067C4.0076 2.50374 3.93817 2.67046 3.9375 2.84449V3.5H3.0625V2.84244C3.06371 2.43705 3.22529 2.04861 3.51195 1.76195C3.79861 1.47529 4.18705 1.31371 4.59244 1.3125L4.59375 1.3125Z" fill="white"/>
+              </svg>),
+            id: '',
+            bg: 'bg-teal-400',
+            textColor: 'text-white'
+        },
+        {
+            title: 'Xuất hợp đồng',
+            svg: (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                <path d="M4.08317 1.74998H2.33317V3.49998H1.1665V0.583313H4.08317V1.74998ZM12.8332 3.49998V0.583313H9.9165V1.74998H11.6665V3.49998H12.8332ZM4.08317 12.25H2.33317V10.5H1.1665V13.4166H4.08317V12.25ZM11.6665 10.5V12.25H9.9165V13.4166H12.8332V10.5H11.6665ZM9.9165 3.49998H4.08317V10.5H9.9165V3.49998ZM11.0832 10.5C11.0832 11.1416 10.5582 11.6666 9.9165 11.6666H4.08317C3.4415 11.6666 2.9165 11.1416 2.9165 10.5V3.49998C2.9165 2.85831 3.4415 2.33331 4.08317 2.33331H9.9165C10.5582 2.33331 11.0832 2.85831 11.0832 3.49998V10.5ZM8.74984 4.66665H5.24984V5.83331H8.74984V4.66665ZM8.74984 6.41665H5.24984V7.58331H8.74984V6.41665ZM8.74984 8.16665H5.24984V9.33331H8.74984V8.16665Z" fill="#4B5563"/>
+              </svg>),
+            id: '',
+            bg: 'bg-white',
+            textColor: 'text-gray-600'
+        },
+        {
+            title: 'Xuất hóa đơn',
+            svg: (<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.84375 3.0625H11.1576C11.563 3.06371 11.9514 3.22529 12.2381 3.51195C12.5247 3.79861 12.6863 4.18705 12.6875 4.59244L12.6875 4.59375L12.6875 8.96875L12.6875 8.97006C12.6863 9.37545 12.5247 9.76389 12.2381 10.0506C11.9514 10.3372 11.563 10.4988 11.1576 10.5L11.1562 10.5H10.5V9.625H11.1556C11.3296 9.62431 11.4963 9.55489 11.6193 9.43183C11.7424 9.30878 11.8118 9.14209 11.8125 8.96808V4.59442C11.8118 4.42041 11.7424 4.25372 11.6193 4.13067C11.4963 4.0076 11.3295 3.93817 11.1555 3.9375H2.84449C2.67046 3.93817 2.50374 4.0076 2.38067 4.13067C2.2576 4.25374 2.18817 4.42046 2.1875 4.59449V8.96801C2.18817 9.14204 2.2576 9.30876 2.38067 9.43183C2.50372 9.55489 2.67041 9.62431 2.84442 9.625H3.5V10.5H2.84375L2.84244 10.5C2.43705 10.4988 2.04861 10.3372 1.76195 10.0506C1.47529 9.76389 1.31371 9.37545 1.3125 8.97006L1.3125 8.96875V4.59244C1.31371 4.18705 1.47529 3.79861 1.76195 3.51195C2.04861 3.22529 2.43705 3.06371 2.84244 3.0625L2.84375 3.0625Z" fill="#4B5563"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.165 7C4.03936 7 3.9375 7.10186 3.9375 7.2275V11.585C3.9375 11.7106 4.03936 11.8125 4.165 11.8125H9.835C9.96064 11.8125 10.0625 11.7106 10.0625 11.585V7.2275C10.0625 7.10186 9.96064 7 9.835 7H4.165ZM3.0625 7.2275C3.0625 6.61861 3.55611 6.125 4.165 6.125H9.835C10.4439 6.125 10.9375 6.61861 10.9375 7.2275V11.585C10.9375 12.1939 10.4439 12.6875 9.835 12.6875H4.165C3.55611 12.6875 3.0625 12.1939 3.0625 11.585V7.2275Z" fill="#4B5563"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.59375 1.3125H9.40756C9.81295 1.31371 10.2014 1.47529 10.4881 1.76195C10.7747 2.04861 10.9363 2.43705 10.9375 2.84244L10.9375 2.84375L10.9375 3.5H10.0625V2.84442C10.0618 2.67041 9.99239 2.50372 9.86933 2.38067C9.74626 2.2576 9.57954 2.18817 9.40551 2.1875H4.59449C4.42046 2.18817 4.25374 2.2576 4.13067 2.38067C4.0076 2.50374 3.93817 2.67046 3.9375 2.84449V3.5H3.0625V2.84244C3.06371 2.43705 3.22529 2.04861 3.51195 1.76195C3.79861 1.47529 4.18705 1.31371 4.59244 1.3125L4.59375 1.3125Z" fill="#4B5563"/>
+              </svg>),
+            id: '',
+            bg: 'bg-white',
+            textColor: 'text-gray-600'
+        },
+    ]
     return (
         <div>
             <HeaderSelect title={'Quản lý tiệc'} slugOrID={id} />
             <RequestBreadcrumbsForQuanLyTiec requestId={id} nameLink={'quan-ly-tiec'} />
+            <div className='flex gap-[10px]'>
+                <div className='ml-auto flex gap-[10px]'>
+                    {buttons.map((button, index) => (
+                        <ButtonCustomAdmin key={index} title={button.title} svg={button.svg} bgColor={button.bg} textColor={button.textColor}></ButtonCustomAdmin>
+
+                    ))}
+                </div>
+            </div>
             <div className='p-4 mt-[30px] w-full bg-white rounded-lg flex flex-col gap-[22px]'>
-                <TitleSpanInfo title={'Thông tin liên hệ'}></TitleSpanInfo>
+                <TitleSpanInfo title={'Thông tin liên hệ'}/>
                 <div className='grid grid-cols-3 gap-[30px]'>
                     {inputInfoUser.map((detail, index) => (
                         <InputDetailCustomer
@@ -359,7 +410,7 @@ const ChiTietTiecCuaChiNhanhPage = ({ params }) => {
                 </div>
             </div>
             <div className='p-4 mt-5 w-full bg-white rounded-lg flex flex-col gap-[22px]'>
-            <TitleSpanInfo title={'Thông tin tổ chức'}></TitleSpanInfo>
+            <TitleSpanInfo title={'Thông tin tổ chức'}/>
                 <div className='grid grid-cols-3 gap-[30px]'>
                     {inputOrganization.map((detail, index) => (
                         <InputDetailCustomer
@@ -398,7 +449,7 @@ const ChiTietTiecCuaChiNhanhPage = ({ params }) => {
                 </div>
             </div>
             <div className='p-4 mt-5 w-full bg-white rounded-lg flex flex-col gap-[22px]'>
-            <TitleSpanInfo title={'Thông tin thanh toán'}></TitleSpanInfo>
+            <TitleSpanInfo title={'Thông tin thanh toán'}/>
                 <div className='grid grid-cols-3 gap-[30px]'>
                     {inputsCost.map((detail, index) => (
                         <InputDetailCustomer
@@ -414,7 +465,7 @@ const ChiTietTiecCuaChiNhanhPage = ({ params }) => {
                 </div>
             </div>
             <div className='p-4 mt-5 w-full bg-white rounded-lg flex flex-col gap-[22px]'>
-                <TitleSpanInfo title={'Bảng chi phí chi tiết'}></TitleSpanInfo>
+                <TitleSpanInfo title={'Bảng chi phí chi tiết'}/>
                 <TableDetail headers={headers} data={data}></TableDetail>
             </div>
             <div className="flex mt-[30px] mr-[30px]">
