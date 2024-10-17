b66637ba6be64044e8904e96882fda987de22b8a
ChuBe04 <118839446+HongSon04@users.noreply.github.com>
2024-10-18 01:24:47 +0700

    [FIX] blog api

--- server/src/blogs/blogs.module.ts
+++ server/src/blogs/blogs.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [BlogsController],
-  providers: [BlogsService],
+  providers: [BlogsService, CloudinaryService, PrismaService],
 })
 export class BlogsModule {}
