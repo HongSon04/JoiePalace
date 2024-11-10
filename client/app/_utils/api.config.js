35a17ce54e68e76540cab9466d9f9336132e8dea
Cao Hoàng Thành <160160995+ThanhDevLagi@users.noreply.github.com>
2024-11-10 15:22:53 +0700

    chỉnh sữa lại phần add quản lý tiệc

--- client/app/_utils/api.config.js
+++ client/app/_utils/api.config.js
@@ -78,6 +78,8 @@ export const API_CONFIG = {
     CREATE: `${STAGES}/create`,
     GET_ALL: (params) =>
       constructUrlWithParams(`${STAGES}/get-all`, params),
+    GET_ALL_BY_BRANCH: (id) => (`${STAGES}/get-all?branch_id=${id}`),
+    GET_ALL_BY_STAGE_ID: (id) => (`${STAGES}/get/${id}`),
   },
 
   // CATEGORIES API
