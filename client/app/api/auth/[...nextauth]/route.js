1364f1a186d616d77a6d32bdbc3c9b8418749394
thanhnq <quocthanhn87@gmail.com>
2024-11-15 13:44:35 +0700

    pull

--- client/app/api/auth/[...nextauth]/route.js
+++ client/app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,22 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-// import {
-//   GOOGLE_ID,
-//   GOOGLE_SECRET,
-//   NEXTAUTH_SECRET,
-// } from "@/app/_utils/googleKey.config";
+import {
+  GOOGLE_ID,
+  GOOGLE_SECRET,
+  NEXTAUTH_SECRET,
+} from "@/app/_utils/googleKey.config";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+      clientId: GOOGLE_ID,
+      clientSecret: GOOGLE_SECRET,
     }),
   ],
   pages: {
     signIn: "/dang-nhap",
   },
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+  secret: NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
